f2f32f45efc39b5e807da228b1498b6c8817e3a6
Michael <zavarella.dev@gmail.com>
2017-01-26 03:54:02 +0000

    Made a hello {{name}} server

--- src/ped/core.clj
+++ src/ped/core.clj
@@ -1,7 +1,27 @@
 (ns ped.core
-  (:gen-class))
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn ok [body]
+  {:status 200 :body body})
+
+(defn greeting [name]
+  (str "Hello, " name "!"))
+
+(defn hello [request]
+  (let [name (get-in request [:query-params :name] "Stranger")
+        response (greeting name)]
+    (ok response)))
+
+(def routes
+  (route/expand-routes
+   #{["/greet" :get hello :route-name :greet]}))
+
+(defn create-server []
+  (http/create-server
+   {::http/routes routes
+    ::http/type   :jetty
+    ::http/port   8890}))
+
+(defn start []
+  (http/start (create-server)))

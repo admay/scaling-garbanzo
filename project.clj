1e24ca7b333df6c3a139ca77bb3c3b9dd6cdc096
Michael <zavarella.dev@gmail.com>
2017-01-26 02:44:13 +0000

    Added deps

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://github.com/admay/scailing-garbanzo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [io.pedestal/pedestal.service "0.5.2"]
+                 [io.pedestal/pedestal.jetty "0.5.2"]
+                 [io.pedestal/pedestal.route "0.5.2"]
+                 [org.slf4j/slf4j-simple "1.7.21"]]
   :main ^:skip-aot ped.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})

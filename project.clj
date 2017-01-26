59fdbb7f8aef242c35f14c22a0dea5bab1ad703e
Michael <zavarella.dev@gmail.com>
2017-01-26 02:40:21 +0000

    Fixed up project metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ped "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Toy project for learning pedestal"
+  :url "http://github.com/admay/scailing-garbanzo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
